fa0644bd88c49feebe93e496d8644e014484a7eb
Doug Baer <dbaer@vmware.com>
2020-04-10 11:55:26 -0700

    adding legacy scripts (Python 2) and other convenience files

---
+++ db/create_db.sql
@@ -0,0 +1,33 @@
+CREATE TABLE 'clients' (
+ "Rank" integer,
+ "Name" varchar(30),
+ "Universe" varchar(25),
+ "Revenue" varchar(20)
+ );
+
+ INSERT INTO 'clients' VALUES
+ (1,'CHOAM','Dune','$1.7 trillion'),
+ (2,'Acme Corp.','Looney Tunes','$348.7 billion'),
+ (3,'Sirius Cybernetics Corp.',"Hitchhiker's Guide",'$327.2 billion'),
+ (4,'Buy n Large','Wall-E','$291.8 billion'),
+ (5,'Aperture Science, Inc.','Valve','$163.4 billion'),
+ (6,'SPECTRE','007','$157.1 billion'),
+ (7,'Very Big Corp. of America','Monty Python','$146.6 billion'),
+ (8,'Frobozz Magic Co.','Zork','$112.9 billion'),
+ (9,'Warbucks Industries',"Lil' Orphan Annie",'$61.5 billion'),
+ (10,'Tyrell Corp.','Bladerunner','$59.4 billion'),
+ (11,'Wayne Enterprises','Batman','$31.3 billion'),
+ (12,'Virtucon','Austin Powers','$24.9 billion'),
+ (13,'Globex','The Simpsons','$23.7 billion'),
+ (14,'Umbrella Corp.','Resident Evil','$22.6 billion'),
+ (15,'Wonka Industries','Charlie and the Chocolate Factory','$21.0 billion'),
+ (16,'Stark Industries','Iron Man','$20.3 billion'),
+ (17,'Clampett Oil','Beverly Hillbillies','$18.1 billion'),
+ (18,'Oceanic Airlines','Lost','$7.8 billion'),
+ (19,'Brawndo','Idiocracy','$5.8 billion'),
+ (20,'Cyberdyne Systems Corp.','Terminator','$5.5 billion'),
+ (21,'Paper Street Soap Company','Fight Club','$5.0 billion'),
+ (22,'Gringotts','Harry Potter','$4.4 billion'),
+ (23,'Oscorp','Spider-Man','$3.1 billion'),
+ (24,'Nakatomi Trading Corp.','Die-Hard','$2.5 billion'),
+ (25,'Los Pollos Hermanos','Breaking Bad','$1.3 billion');
